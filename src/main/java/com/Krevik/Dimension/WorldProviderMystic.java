c1e6951318526020fae0a6d285507d1c08d3781b
DESKTOP-3KV8A1L\Krevik <kczyku.kw@gmail.com>
2018-08-19 17:01:58 +0200

    Changes in sky color

--- src/main/java/com/Krevik/Dimension/WorldProviderMystic.java
+++ src/main/java/com/Krevik/Dimension/WorldProviderMystic.java
@@ -4,7 +4,6 @@ import javax.annotation.Nullable;
 
 import com.Krevik.Main.KCore;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
@@ -14,7 +13,6 @@ import net.minecraft.world.biome.BiomeProvider;
 import net.minecraft.world.border.WorldBorder;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.client.IRenderHandler;
-import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -68,7 +66,60 @@ public class WorldProviderMystic extends WorldProviderSurface
     @SideOnly(Side.CLIENT)
     public Vec3d getSkyColor(net.minecraft.entity.Entity cameraEntity, float partialTicks)
     {
-        return new Vec3d(2.37,1.65,0.71);
+    		//return new Vec3d(0.237,0.165,0.071);
+            float f = world.getCelestialAngle(partialTicks);
+            float f1 = MathHelper.cos(f * ((float)Math.PI * 2F)) * 2.0F + 0.5F;
+            f1 = MathHelper.clamp(f1, 0.0F, 1.0F);
+            int i = MathHelper.floor(cameraEntity.posX);
+            int j = MathHelper.floor(cameraEntity.posY);
+            int k = MathHelper.floor(cameraEntity.posZ);
+            BlockPos blockpos = new BlockPos(i, j, k);
+            int l = net.minecraftforge.client.ForgeHooksClient.getSkyBlendColour(world, blockpos);
+            float f3 = (float)(l >> 16 & 255) / 255.0F;
+            float f4 = (float)(l >> 8 & 255) / 255.0F;
+            float f5 = (float)(l & 255) / 255.0F;
+            f3 = (float) (f3 * f1*0.237);
+            f4 = (float) (f4 * f1*0.165);
+            f5 = (float) (f5 * f1*0.071);
+            float f6 = world.getRainStrength(partialTicks);
+
+            if (f6 > 0.0F)
+            {
+                float f7 = (f3 * 0.3F + f4 * 0.59F + f5 * 0.11F) * 0.6F;
+                float f8 = 1.0F - f6 * 0.75F;
+                f3 = f3 * f8 + f7 * (1.0F - f8);
+                f4 = f4 * f8 + f7 * (1.0F - f8);
+                f5 = f5 * f8 + f7 * (1.0F - f8);
+            }
+
+            float f10 = world.getThunderStrength(partialTicks);
+
+            if (f10 > 0.0F)
+            {
+                float f11 = (f3 * 0.3F + f4 * 0.59F + f5 * 0.11F) * 0.2F;
+                float f9 = 1.0F - f10 * 0.75F;
+                f3 = f3 * f9 + f11 * (1.0F - f9);
+                f4 = f4 * f9 + f11 * (1.0F - f9);
+                f5 = f5 * f9 + f11 * (1.0F - f9);
+            }
+
+            if (world.getLastLightningBolt() > 0)
+            {
+                float f12 = (float)world.getLastLightningBolt() - partialTicks;
+
+                if (f12 > 1.0F)
+                {
+                    f12 = 1.0F;
+                }
+
+                f12 = f12 * 0.45F;
+                f3 = f3 * (1.0F - f12) + 0.8F * f12;
+                f4 = f4 * (1.0F - f12) + 0.8F * f12;
+                f5 = f5 * (1.0F - f12) + 1.0F * f12;
+            }
+
+            return new Vec3d((double)f3, (double)f4, (double)f5);
+
     }
     
     @SideOnly(Side.CLIENT)
@@ -82,7 +133,7 @@ public class WorldProviderMystic extends WorldProviderSurface
         f1 = f1 * (f * 0.94F + 0.06F);
         f2 = f2 * (f * 0.94F + 0.06F);
         f3 = f3 * (f * 0.91F + 0.09F);
-        return new Vec3d((double)f1, (double)f2, (double)f3);
+        return new Vec3d((double)f1/5, (double)f2/5, (double)f3/5);
     }
 
     public IChunkGenerator createChunkGenerator()
