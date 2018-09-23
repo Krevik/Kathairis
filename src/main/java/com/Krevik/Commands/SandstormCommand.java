0195eb51a8726ccc49f23fb3557c96a0c34a30ab
DESKTOP-3KV8A1L\Krevik <kczyku.kw@gmail.com>
2018-09-23 11:08:55 +0200

    Fixed sandstorm command

--- src/main/java/com/Krevik/Commands/SandstormCommand.java
+++ src/main/java/com/Krevik/Commands/SandstormCommand.java
@@ -32,12 +32,11 @@ public class SandstormCommand extends CommandBase{
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
 		if(args.length<=0) {
-            throw new WrongUsageException("usage: /sandstorm <time>", new Object[0]);
+            throw new WrongUsageException("/sandstorm <time>", new Object[0]);
 		}else {
 			World world = sender.getEntityWorld();
 			KetherDataStorage data = KetherDataStorage.getDataInstance(world);
 			if(!world.isRemote) {
-				if(DimensionManager.getWorld(KCore.instance.DIMENSION_ID)==world) {
 					if(data!=null) {
 						if(args[0]!=null) {
 							if(Integer.parseInt(args[0])==0) {
@@ -56,9 +55,6 @@ public class SandstormCommand extends CommandBase{
 					}else {
 			            throw new WrongUsageException("Kathairis data storage is not loaded!", new Object[0]);	
 					}
-				}else {
-		            throw new WrongUsageException("In order to use this command you must be in kathairis!", new Object[0]);
-				}
 			}else {
 	            throw new WrongUsageException("Cannot proccess on client side!", new Object[0]);
 			}
