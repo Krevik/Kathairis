73d6fbf728e7ae2709d8bd07bdf037fcc90f8aa3
DESKTOP-3KV8A1L\Krevik <kczyku.kw@gmail.com>
2018-09-25 23:13:38 +0200

    Imports organized

--- src/main/java/com/Krevik/Main/ClientProxy.java
+++ src/main/java/com/Krevik/Main/ClientProxy.java
@@ -1,13 +1,8 @@
 package com.Krevik.Main;
 
-import java.util.Map;
-
-import javax.annotation.Nullable;
-
 import com.Krevik.Particles.TextureStitcherParicleManager;
 import com.Krevik.TileEntity.TileEntityMythicStoneSign;
 import com.Krevik.TileEntity.TileEntityMythicStoneSignRenderer;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
@@ -16,8 +11,12 @@ import net.minecraft.client.particle.Particle;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.DefaultStateMapper;
 import net.minecraft.client.renderer.block.statemap.IStateMapper;
+import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ColorizerGrass;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.client.EnumHelperClient;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -31,6 +30,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.registries.IRegistryDelegate;
+
+import java.util.Map;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
