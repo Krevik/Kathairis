d908b37e79fea8c54fe28788f68679e7df61a992
DESKTOP-3KV8A1L\Krevik <kczyku.kw@gmail.com>
2018-10-10 06:54:02 +0200

    Some fixes with sandstorm on SMP

---
+++ src/main/java/com/Krevik/Networking/PacketSandstormUpdatedOnClient.java
@@ -0,0 +1,59 @@
+package com.Krevik.Networking;
+
+import com.Krevik.Dimension.KetherDataStorage;
+import com.Krevik.Main.KCore;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+public class PacketSandstormUpdatedOnClient implements IMessage {
+
+    private boolean isSandstorm;
+    private float mX;
+    private float sandstormTime;
+    private float mZ;
+
+    public PacketSandstormUpdatedOnClient() {
+    }
+
+    public PacketSandstormUpdatedOnClient(boolean isSandstorm1, float mx,float sandstormtime,float mz) {
+        isSandstorm=isSandstorm1;
+        this.mX=mx;
+        this.sandstormTime=sandstormtime;
+        this.mZ=mz;
+    }
+
+    @Override
+    public void fromBytes(ByteBuf buf) {
+        isSandstorm=buf.readBoolean();
+        mX=buf.readFloat();
+        sandstormTime=buf.readFloat();
+        mZ=buf.readFloat();
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf) {
+        buf.writeBoolean(isSandstorm);
+        buf.writeFloat(mX);
+        buf.writeFloat(sandstormTime);
+        buf.writeFloat(mZ);
+    }
+
+    public static class Handler implements IMessageHandler<PacketSandstormUpdatedOnClient, IMessage> {
+
+        @Override
+        public IMessage onMessage(PacketSandstormUpdatedOnClient message, MessageContext ctx) {
+            EntityPlayer ep=Minecraft.getMinecraft().player;
+            KetherDataStorage storage = KetherDataStorage.getDataInstance(ep.world);
+            storage.setIsSandstorm(message.isSandstorm);
+            storage.setSandstormX(message.mX);
+            storage.setSandstormTime((int)message.sandstormTime);
+            storage.setSandstormZ(message.mZ);
+            return null;
+        }
+    }
+}
