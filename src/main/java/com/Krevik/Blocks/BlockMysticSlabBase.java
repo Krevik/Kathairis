7c632cd341a926f3062fa1aa8259ccd36cf50149
DESKTOP-3KV8A1L\Krevik <kczyku.kw@gmail.com>
2018-10-15 09:24:16 +0200

    Fixed slabs bugs

--- src/main/java/com/Krevik/Blocks/BlockMysticSlabBase.java
+++ src/main/java/com/Krevik/Blocks/BlockMysticSlabBase.java
@@ -23,10 +23,14 @@ import net.minecraft.client.renderer.block.statemap.DefaultStateMapper;
 import net.minecraft.client.renderer.block.statemap.IStateMapper;
 import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
@@ -88,12 +92,48 @@ public class BlockMysticSlabBase extends BlockSlab {
 
 	@Override
 	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-		return Item.getItemFromBlock(KCore.MysticWoodHalfSlab);
+		if(state.getBlock()==KCore.MysticWoodHalfSlab||state.getBlock()==KCore.MysticWoodDoubleSlab) {
+			return Item.getItemFromBlock(KCore.MysticWoodHalfSlab);
+		}
+		else if(state.getBlock()==KCore.ShinyWoodHalfSlab||state.getBlock()==KCore.ShinyWoodDoubleSlab){
+			return Item.getItemFromBlock(KCore.ShinyWoodHalfSlab);
+		}
+		else if(state.getBlock()==KCore.SoulWoodHalfSlab||state.getBlock()==KCore.SoulWoodDoubleSlab){
+			return Item.getItemFromBlock(KCore.SoulWoodHalfSlab);
+		}else{
+			return Item.getItemFromBlock(KCore.MysticWoodHalfSlab);
+		}
 	}
 
 	@Override
 	protected ItemStack getSilkTouchDrop(IBlockState state) {
-		return new ItemStack(Item.getItemFromBlock(KCore.MysticWoodHalfSlab), 2, 0);
+		if(state.getBlock()==KCore.MysticWoodHalfSlab||state.getBlock()==KCore.MysticWoodDoubleSlab) {
+			return new ItemStack(Item.getItemFromBlock(KCore.MysticWoodHalfSlab), 2, 0);
+		}
+		else if(state.getBlock()==KCore.ShinyWoodHalfSlab||state.getBlock()==KCore.ShinyWoodDoubleSlab) {
+			return new ItemStack(Item.getItemFromBlock(KCore.ShinyWoodHalfSlab), 2, 0);
+		}
+		else if(state.getBlock()==KCore.SoulWoodHalfSlab||state.getBlock()==KCore.SoulWoodDoubleSlab) {
+			return new ItemStack(Item.getItemFromBlock(KCore.SoulWoodHalfSlab), 2, 0);
+		}else{
+			return new ItemStack(Item.getItemFromBlock(KCore.MysticWoodHalfSlab), 2, 0);
+		}
+	}
+
+	@Override
+	public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+		if(state.getBlock()==KCore.MysticWoodDoubleSlab||state.getBlock()==KCore.MysticWoodHalfSlab){
+			return new ItemStack(KCore.MysticWoodHalfSlab);
+		}
+		else if(state.getBlock()==KCore.ShinyWoodDoubleSlab||state.getBlock()==KCore.ShinyWoodHalfSlab){
+			return new ItemStack(KCore.ShinyWoodHalfSlab);
+		}
+		else if(state.getBlock()==KCore.SoulWoodDoubleSlab||state.getBlock()==KCore.SoulWoodHalfSlab){
+			return new ItemStack(KCore.SoulWoodHalfSlab);
+		}
+		else{
+			return new ItemStack(KCore.MysticWoodHalfSlab);
+		}
 	}
 
 	@Override
