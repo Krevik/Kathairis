ab1f41aa8c5ea2778059cba82bbdb7ed899934d9
DESKTOP-3KV8A1L\Krevik <kczyku.kw@gmail.com>
2018-10-17 06:59:14 +0200

    Fixes with light and player can use bed now

--- src/main/java/com/Krevik/Dimension/WorldProviderMystic.java
+++ src/main/java/com/Krevik/Dimension/WorldProviderMystic.java
@@ -10,7 +10,6 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProviderSurface;
 import net.minecraft.world.biome.BiomeProvider;
-import net.minecraft.world.border.WorldBorder;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.client.IRenderHandler;
 import net.minecraftforge.fml.relauncher.Side;
@@ -30,6 +29,33 @@ public class WorldProviderMystic extends WorldProviderSurface
         this.biomeProvider = new BiomeProviderMystic(world.getWorldInfo().getSeed(),world.getWorldType());
         this.generateLightBrightnessTable();
         this.hasSkyLight = true;
+    }
+
+    public boolean shouldClientCheckLighting()
+    {
+        return true;
+    }
+
+    public WorldSleepResult canSleepAt(net.minecraft.entity.player.EntityPlayer player, BlockPos pos)
+    {
+        return WorldSleepResult.ALLOW;
+    }
+    public float getSunBrightnessFactor(float par1)
+    {
+        return world.getSunBrightnessFactor(par1);
+    }
+
+
+
+    protected void generateLightBrightnessTable()
+    {
+        float f = 0.0F;
+
+        for (int i = 0; i <= 15; ++i)
+        {
+            float f1 = 1.0F - (float)i / 15.0F;
+            this.lightBrightnessTable[i] = (1.0F - f1) / (f1 * 3.0F + 1.0F) * 1.0F + 0.0F;
+        }
     }
 
     
@@ -183,21 +209,6 @@ public class WorldProviderMystic extends WorldProviderSurface
         return false;
     }
 
-
-    public WorldBorder createWorldBorder()
-    {
-        return new WorldBorder()
-        {
-            public double getCenterX()
-            {
-                return super.getCenterX() / 8.0D;
-            }
-            public double getCenterZ()
-            {
-                return super.getCenterZ() / 8.0D;
-            }
-        };
-    }
 
     public DimensionType getDimensionType()
     {
