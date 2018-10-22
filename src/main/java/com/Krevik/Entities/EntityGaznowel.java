ec44897006f37f6bec4b57a827121f1b7c04b6eb
DESKTOP-3KV8A1L\Krevik <kczyku.kw@gmail.com>
2018-10-22 08:10:15 +0200

    Improved gaznowel AI and animation

--- src/main/java/com/Krevik/Entities/EntityGaznowel.java
+++ src/main/java/com/Krevik/Entities/EntityGaznowel.java
@@ -1,39 +1,103 @@
 package com.Krevik.Entities;
 
-import net.minecraft.entity.EntityFlying;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.EntityAIFindEntityNearestPlayer;
-import net.minecraft.entity.ai.EntityMoveHelper;
-import net.minecraft.entity.monster.EntityGhast;
+import com.Krevik.Main.MysticLootTables;
+import net.minecraft.entity.*;
+import net.minecraft.entity.ai.*;
+import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.entity.projectile.EntityTippedArrow;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvent;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.loot.LootTableList;
 
 import javax.annotation.Nullable;
+import javax.vecmath.Vector3d;
 
-public class EntityGaznowel extends EntityFlying {
+public class EntityGaznowel extends EntityFlying implements IRangedAttackMob, IMob {
 
+    private static final DataParameter<Boolean> SWINGING_ARMS = EntityDataManager.<Boolean>createKey(EntityGaznowel.class, DataSerializers.BOOLEAN);
+    private boolean hasMovementVector=false;
+    private Vector3d movementVector=new Vector3d(0,0,0);
     public EntityGaznowel(World worldIn)
     {
         super(worldIn);
-        this.setSize(1.5F, 2.0F);
+        this.setSize(1F, 2.0F);
         this.experienceValue = 35;
         this.moveHelper = new EntityGaznowel.GaznowelMoveHelper(this);
+
+    }
+
+    public boolean getIsSwingingArms(){
+        return dataManager.get(SWINGING_ARMS).booleanValue();
     }
 
     protected void initEntityAI()
     {
         this.targetTasks.addTask(1, new EntityAIFindEntityNearestPlayer(this));
+        this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 25.0F));
+        this.targetTasks.addTask(2, new EntityGaznowel.AIInteractWithPlayer(this));
+        this.tasks.addTask(2, new EntityGaznowel.AIWander(this));
+
     }
 
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3000000417232513D);
+        this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(4.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(30.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(20.0D);
+    }
+
+    public void attackEntityWithRangedAttack(EntityLivingBase target, float distanceFactor)
+    {
+        EntityArrow entityarrow = this.getArrow(distanceFactor);
+        double d0 = target.posX - this.posX;
+        double d1 = target.getEntityBoundingBox().minY + (double)(target.height / 3.0F) - entityarrow.posY;
+        double d2 = target.posZ - this.posZ;
+        double d3 = (double)MathHelper.sqrt(d0 * d0 + d2 * d2);
+        entityarrow.shoot(d0, d1 + d3 * 0.20000000298023224D, d2, 1.6F, (float)(14 - this.world.getDifficulty().getDifficultyId() * 4));
+        this.playSound(SoundEvents.ENTITY_SKELETON_SHOOT, 1.0F, 1.0F / (this.getRNG().nextFloat() * 0.4F + 0.8F));
+        this.world.spawnEntity(entityarrow);
+    }
+
+    protected EntityArrow getArrow(float p_190726_1_)
+    {
+        EntityTippedArrow entitytippedarrow = new EntityTippedArrow(this.world, this);
+        entitytippedarrow.setEnchantmentEffectsFromEntity(this, p_190726_1_);
+        return entitytippedarrow;
+    }
+
+   /* public void setCombatTask()
+    {
+        if (this.world != null && !this.world.isRemote)
+        {
+            this.tasks.removeTask(this.aiArrowAttack);
+            ItemStack itemstack = this.getHeldItemMainhand();
+
+                int i = 20;
+
+                this.aiArrowAttack.setAttackCooldown(i);
+                this.tasks.addTask(4, this.aiArrowAttack);
+        }
+    }*/
+
+   int timer=0;
     public void onUpdate()
     {
         super.onUpdate();
@@ -42,17 +106,73 @@ public class EntityGaznowel extends EntityFlying {
         {
             this.setDead();
         }
+
+        if(movementVector.x-posX<0.001&&movementVector.y-posY<0.001&&movementVector.z-posZ<0.001){
+            hasMovementVector=false;
+        }
+        if(hasMovementVector){
+            motionX=(movementVector.x-posX)*0.01f;
+            motionY=(movementVector.y-posY)*0.01f;
+            motionZ=(movementVector.z-posZ)*0.01f;
+        }
+        if(getAttackTarget()!=null){
+            dataManager.set(SWINGING_ARMS,Boolean.valueOf(true));
+        }else{
+            dataManager.set(SWINGING_ARMS,Boolean.valueOf(false));
+        }
+        timer++;
+        if(timer>=10){
+            timer=0;
+            EntityPlayer ep = world.getNearestAttackablePlayer(this,15,15);
+            if(ep!=null){
+                setAttackTarget(ep);
+            }else{
+                setAttackTarget(null);
+            }
+        }
     }
 
+    BlockPos findPositionNearPlayer(EntityGaznowel gaznowel){
+        BlockPos result;
+        double posX=gaznowel.getRNG().nextInt(2)==0?gaznowel.getAttackTarget().posX-5-gaznowel.getRNG().nextInt(6):gaznowel.getAttackTarget().posX+5+gaznowel.getRNG().nextInt(6);
+        double posY=gaznowel.getAttackTarget().posY+gaznowel.getRNG().nextInt(8);
+        double posZ=gaznowel.getRNG().nextInt(2)==0?gaznowel.getAttackTarget().posZ-5-gaznowel.getRNG().nextInt(6):gaznowel.getAttackTarget().posZ+5+gaznowel.getRNG().nextInt(6);
+        result=new BlockPos(posX,posY,posZ);
+        if(!(gaznowel.world.isAirBlock(result)&&gaznowel.world.isAirBlock(result.up()))){
+            return findPositionNearPlayer(gaznowel);
+        }else{
+            return result;
+        }
+    }
+
+    BlockPos findSomePosition(EntityGaznowel gaznowel){
+        BlockPos result;
+        double posX=gaznowel.posX-5+gaznowel.getRNG().nextInt(10);
+        double posZ=gaznowel.posZ-5+gaznowel.getRNG().nextInt(10);
+        double posY=gaznowel.posY-5+gaznowel.getRNG().nextInt(10);
+        result=new BlockPos(posX,posY,posZ);
+        if(!(gaznowel.world.isAirBlock(result)&&gaznowel.world.isAirBlock(result.up()))){
+            return findSomePosition(gaznowel);
+        }else{
+            return result;
+        }
+    }
     public boolean attackEntityFrom(DamageSource source, float amount)
     {
+        if(getAttackTarget()!=null){
+            BlockPos target = findPositionNearPlayer(this);
+            movementVector = new Vector3d(target.getX(), target.getY(), target.getZ());
+        }else {
+            BlockPos target = findSomePosition(this);
+            movementVector = new Vector3d(target.getX(), target.getY(), target.getZ());
+        }
         if (this.isEntityInvulnerable(source))
         {
             return false;
         }
         else if (source.getTrueSource() instanceof EntityPlayer)
         {
-            super.attackEntityFrom(source, 1000.0F);
+            super.attackEntityFrom(source, amount);
             return true;
         }
         else
@@ -61,17 +181,25 @@ public class EntityGaznowel extends EntityFlying {
         }
     }
 
+    protected void setEquipmentBasedOnDifficulty(DifficultyInstance difficulty)
+    {
+        super.setEquipmentBasedOnDifficulty(difficulty);
+        this.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, new ItemStack(Items.BOW));
+    }
+    @Nullable
+    public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata) {
+        livingdata = super.onInitialSpawn(difficulty, livingdata);
+        this.setEquipmentBasedOnDifficulty(difficulty);
+        this.setHeldItem(EnumHand.MAIN_HAND, new ItemStack(Items.BOW));
+        return livingdata;
+    }
+
     protected void entityInit()
     {
         super.entityInit();
+        this.dataManager.register(SWINGING_ARMS, Boolean.valueOf(false));
     }
 
-    protected void applyEntityAttributes()
-    {
-        super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(30.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(15.0D);
-    }
 
     public SoundCategory getSoundCategory()
     {
@@ -96,7 +224,7 @@ public class EntityGaznowel extends EntityFlying {
     @Nullable
     protected ResourceLocation getLootTable()
     {
-        return LootTableList.ENTITIES_GHAST;
+        return MysticLootTables.LOOT_GAZNOWEL;
     }
 
     protected float getSoundVolume()
@@ -122,6 +250,12 @@ public class EntityGaznowel extends EntityFlying {
     public void readEntityFromNBT(NBTTagCompound compound)
     {
         super.readEntityFromNBT(compound);
+    }
+
+
+    @Override
+    public void setSwingingArms(boolean swingingArms) {
+        this.dataManager.set(SWINGING_ARMS, Boolean.valueOf(swingingArms));
     }
 
     static class GaznowelMoveHelper extends EntityMoveHelper
@@ -186,5 +320,152 @@ public class EntityGaznowel extends EntityFlying {
             return true;
         }
     }
+
+    static class AIInteractWithPlayer extends EntityAIBase
+    {
+        private final EntityGaznowel gaznowel;
+
+        public AIInteractWithPlayer(EntityGaznowel p_i45859_1_)
+        {
+            this.gaznowel = p_i45859_1_;
+            this.setMutexBits(3);
+        }
+
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        public boolean shouldExecute()
+        {
+            return gaznowel.getAttackTarget()!=null;
+        }
+
+        /**
+         * Keep ticking a continuous task that has already been started
+         */
+        private int seeTime=0;
+        private final float maxAttackDistance=10;
+        private int attackTime = -1;
+        private int strafingTime = -1;
+        private final double moveSpeedAmp=1;
+        private int attackCooldown=15;
+        public void updateTask() {
+            gaznowel.getLookHelper().setLookPositionWithEntity(gaznowel.getAttackTarget(), 10.0F, 100F);
+            gaznowel.faceEntity(gaznowel.getAttackTarget(),1000,1000);
+            gaznowel.getLookHelper().onUpdateLook();
+            if(gaznowel.getDistance(gaznowel.getAttackTarget())>8F||!gaznowel.hasMovementVector||gaznowel.getRNG().nextInt(100)==0){
+                BlockPos targetPos = findPositionNearPlayer(gaznowel);
+                gaznowel.movementVector=new Vector3d(targetPos.getX(),targetPos.getY(),targetPos.getZ());
+                gaznowel.hasMovementVector=true;
+            }
+            EntityLiving entity = gaznowel;
+            EntityLivingBase entitylivingbase = entity.getAttackTarget();
+
+            if (entitylivingbase != null)
+            {
+                gaznowel.getDataManager().set(gaznowel.SWINGING_ARMS,true);
+                double d0 = entity.getDistanceSq(entitylivingbase.posX, entitylivingbase.getEntityBoundingBox().minY, entitylivingbase.posZ);
+                boolean flag = entity.getEntitySenses().canSee(entitylivingbase);
+                boolean flag1 = this.seeTime > 0;
+
+                if (flag != flag1)
+                {
+                    this.seeTime = 0;
+                }
+
+                if (flag)
+                {
+                    ++this.seeTime;
+                }
+                else
+                {
+                    --this.seeTime;
+                }
+
+                if (entity.isHandActive())
+                {
+                    if (!flag && this.seeTime < -60)
+                    {
+                        entity.resetActiveHand();
+                    }
+                    else if (flag)
+                    {
+                        int i = entity.getItemInUseMaxCount();
+
+                        if (i >= 15)
+                        {
+                            entity.resetActiveHand();
+                            ((IRangedAttackMob)entity).attackEntityWithRangedAttack(entitylivingbase, ItemBow.getArrowVelocity(i));
+                            this.attackTime = this.attackCooldown;
+                        }
+                    }
+                }
+                else if (--this.attackTime <= 0 && this.seeTime >= -60)
+                {
+                    entity.setActiveHand(EnumHand.MAIN_HAND);
+                }
+            }
+        }
+
+        BlockPos findPositionNearPlayer(EntityGaznowel gaznowel){
+            BlockPos result;
+            double posX=gaznowel.getRNG().nextInt(2)==0?gaznowel.getAttackTarget().posX-5-gaznowel.getRNG().nextInt(6):gaznowel.getAttackTarget().posX+5+gaznowel.getRNG().nextInt(6);
+            double posY=gaznowel.getAttackTarget().posY+gaznowel.getRNG().nextInt(8);
+            double posZ=gaznowel.getRNG().nextInt(2)==0?gaznowel.getAttackTarget().posZ-5-gaznowel.getRNG().nextInt(6):gaznowel.getAttackTarget().posZ+5+gaznowel.getRNG().nextInt(6);
+            result=new BlockPos(posX,posY,posZ);
+            if(!(gaznowel.world.isAirBlock(result)&&gaznowel.world.isAirBlock(result.up()))){
+                return findPositionNearPlayer(gaznowel);
+            }else{
+                return result;
+            }
+        }
+    }
+
+    static class AIWander extends EntityAIBase
+    {
+        private final EntityGaznowel gaznowel;
+
+        public AIWander(EntityGaznowel p_i45859_1_)
+        {
+            this.gaznowel = p_i45859_1_;
+            this.setMutexBits(3);
+        }
+
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        public boolean shouldExecute()
+        {
+            return gaznowel.getAttackTarget()==null;
+        }
+
+        /**
+         * Keep ticking a continuous task that has already been started
+         */
+        public void updateTask() {
+
+            if(!gaznowel.hasMovementVector&&gaznowel.getRNG().nextInt(500)==0){
+                BlockPos targetPos = findSomePosition(gaznowel);
+                gaznowel.movementVector=new Vector3d(targetPos.getX(),targetPos.getY(),targetPos.getZ());
+                gaznowel.hasMovementVector=true;
+                gaznowel.getLookHelper().setLookPosition((float)gaznowel.movementVector.x,(float)gaznowel.movementVector.y,(float)gaznowel.movementVector.z,40,100);
+                gaznowel.getLookHelper().onUpdateLook();
+            }
+        }
+
+        BlockPos findSomePosition(EntityGaznowel gaznowel){
+            BlockPos result;
+            double posX=gaznowel.posX-5+gaznowel.getRNG().nextInt(10);
+            double posZ=gaznowel.posZ-5+gaznowel.getRNG().nextInt(10);
+            double posY=gaznowel.posY-5+gaznowel.getRNG().nextInt(10);
+            result=new BlockPos(posX,posY,posZ);
+            if(!(gaznowel.world.isAirBlock(result)&&gaznowel.world.isAirBlock(result.up()))){
+                return findSomePosition(gaznowel);
+            }else{
+                return result;
+            }
+        }
+    }
+
+
 
 }
--- src/main/java/com/Krevik/Renderers/RenderGaznowel.java
+++ src/main/java/com/Krevik/Renderers/RenderGaznowel.java
@@ -5,9 +5,19 @@ import com.Krevik.Entities.EntityGaznowel;
 import com.Krevik.Main.EntityAndRenderRegistry;
 import com.Krevik.Models.ModelBigTurtle;
 import com.Krevik.Models.ModelGaznowel;
+import com.Krevik.Renderers.Layer.RenderLayerHeldItem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.RenderSkeleton;
+import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHandSide;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.relauncher.Side;
@@ -15,22 +25,20 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
-public class RenderGaznowel extends RenderLiving<EntityGaznowel>
-{
+public class RenderGaznowel extends RenderLiving<EntityGaznowel> {
 
     public static final Factory FACTORY = new Factory();
 
-    public RenderGaznowel(RenderManager renderManagerIn)
-    {
-        super(renderManagerIn, new ModelGaznowel(), 0.8F);
+    public RenderGaznowel(RenderManager renderManagerIn) {
+        super(renderManagerIn, new ModelGaznowel(), 0.0F);
+        this.addLayer(new RenderLayerHeldItem(this));
     }
 
 
     /**
      * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
      */
-    protected ResourceLocation getEntityTexture(EntityGaznowel entity)
-    {
+    protected ResourceLocation getEntityTexture(EntityGaznowel entity) {
         return EntityAndRenderRegistry.GaznowelLoc;
     }
 
@@ -43,13 +51,15 @@ public class RenderGaznowel extends RenderLiving<EntityGaznowel>
 
     }
 
-    protected void applyRotations(EntityGaznowel entityLiving, float p_77043_2_, float rotationYaw, float partialTicks)
-    {
+    protected void applyRotations(EntityGaznowel entityLiving, float p_77043_2_, float rotationYaw, float partialTicks) {
         GL11.glEnable(GL11.GL_NORMALIZE);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
         super.applyRotations(entityLiving, p_77043_2_, rotationYaw, partialTicks);
 
     }
+
+
+
 
 }
