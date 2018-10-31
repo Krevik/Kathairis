0828149a10b287b1426cf1c4f2a142438f2105d3
DESKTOP-3KV8A1L\Krevik <kczyku.kw@gmail.com>
2018-10-31 06:14:23 +0100

    SMP fixes

--- src/main/java/com/Krevik/Potion/StunPotion.java
+++ src/main/java/com/Krevik/Potion/StunPotion.java
@@ -10,8 +10,6 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 public class StunPotion extends Potion {
 
-    private static final ResourceLocation texture = KCore.instance.cproxy.stun_potion;
-
     public StunPotion(boolean isBadEffectIn, int liquidColorIn) {
         super(isBadEffectIn, liquidColorIn);
     }
@@ -19,14 +17,14 @@ public class StunPotion extends Potion {
     @Override
     @SideOnly(Side.CLIENT)
     public void renderHUDEffect(int x, int y, PotionEffect effect, Minecraft mc, float alpha) {
-        mc.renderEngine.bindTexture(texture);
+        mc.renderEngine.bindTexture(KCore.instance.cproxy.stun_potion);
         net.minecraft.client.gui.Gui.drawModalRectWithCustomSizedTexture(x + 3, y + 3, 0, 0, 16, 16, 16, 16);
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc) {
-        mc.renderEngine.bindTexture(texture);
+        mc.renderEngine.bindTexture(KCore.instance.cproxy.stun_potion);
         net.minecraft.client.gui.Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0, 0, 16, 16, 16, 16);
     }
 
