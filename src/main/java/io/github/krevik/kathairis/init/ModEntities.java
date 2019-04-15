a9f117daea2c31e56e52ca72fd8f98630ee669df
DESKTOP-3KV8A1L\Krevik <kczyku.kw@gmail.com>
2019-04-15 16:05:17 +0200

    entities spawns tests

--- src/main/java/io/github/krevik/kathairis/init/ModEntities.java
+++ src/main/java/io/github/krevik/kathairis/init/ModEntities.java
@@ -51,35 +51,35 @@ public class ModEntities {
     }
 
     public static void registerPlacementTypes(){
-        registerPlacementType(EntityType.getById(ModEntities.BASIC_BUTTERFLY1.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.BASIC_BUTTERFLY2.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.CLOUD_SHIMMER.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.ILLUKINI.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.RUBY_SILE.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.SKYLIGHT.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.BIG_TURTLE.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.BISON.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.CACTI_SPORE.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.CAMEL.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.CLOUD_OISTER.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.CLOUDY_SLIME.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.FLYING_SQUID.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.FUNGITE.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.GAZNOWEL.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.GECKO.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.HOWLER.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.JELLY_FISH.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.LIVING_FLOWER.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.MYSTIC_BIRD.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.PHASM.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.POISONOUS_SCORPION.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.SKYRAY.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
-        registerPlacementType(EntityType.getById(ModEntities.STRANGE_WANDERER.getRegistryName().toString()), EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.BASIC_BUTTERFLY1, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.BASIC_BUTTERFLY2, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.CLOUD_SHIMMER, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.ILLUKINI, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.RUBY_SILE, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.SKYLIGHT, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.BIG_TURTLE, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.BISON, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.CACTI_SPORE, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.CAMEL, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.CLOUD_OISTER, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.CLOUDY_SLIME, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.FLYING_SQUID, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.FUNGITE, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.GAZNOWEL, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.GECKO, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.HOWLER, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.JELLY_FISH, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.LIVING_FLOWER, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.MYSTIC_BIRD, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.PHASM, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.POISONOUS_SCORPION, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.SKYRAY, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
+        registerPlacementType(ModEntities.STRANGE_WANDERER, EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND);
     }
 
     private static void registerEntitySpawn(EntityType<? extends EntityLiving> type,EnumCreatureType creatureType, EntitySpawnPlacementRegistry.SpawnPlacementType spawnType,Biome[] biomes, int weight, int min, int max) {
         for (Biome b : biomes) {
-            Biome biome = ForgeRegistries.BIOMES.getValue(b.getRegistryName());
+            Biome biome=b;
             if (biome != null) {
                 biome.getSpawns(creatureType).add(new Biome.SpawnListEntry(type, weight, min, max));
             }
@@ -88,19 +88,19 @@ public class ModEntities {
 
     public static void registerEntitySpawns(){
         EntitySpawnPlacementRegistry.SpawnPlacementType ON_GROUND = EntitySpawnPlacementRegistry.SpawnPlacementType.ON_GROUND;
-        registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.MYSTIC_BIRD.getRegistryName().toString()),EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST,ModBiomes.KATHARIAN_SWAMP,ModBiomes.PLAIN_FIELDS},12,1,2);
-        registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.BIG_TURTLE.getRegistryName().toString()),EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_DESERT,ModBiomes.KATHARIAN_DESERT_EDGE,ModBiomes.SOFT_SAND_LAKES},12,1,1);
-        registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.POISONOUS_SCORPION.getRegistryName().toString()),EnumCreatureType.MONSTER,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_DESERT,ModBiomes.KATHARIAN_DESERT_EDGE,ModBiomes.SOFT_SAND_LAKES},3,1,1);
-        registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.CAMEL.getRegistryName().toString()),EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_DESERT,ModBiomes.KATHARIAN_DESERT_EDGE,ModBiomes.SOFT_SAND_LAKES},6,1,1);
-        registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.GECKO.getRegistryName().toString()),EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST},4,1,1);
-        registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.LIVING_FLOWER.getRegistryName().toString()),EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST},8,1,1);
-        registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.HOWLER.getRegistryName().toString()),EnumCreatureType.MONSTER,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST},5,1,1);
-        registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.FUNGITE.getRegistryName().toString()),EnumCreatureType.MONSTER,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST},2,1,1);
-        //registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.CACTI_SPORE.getRegistryName().toString()),EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST},4,1,1);
-        //registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.JELLY_FISH.getRegistryName().toString()),EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.PLAIN_FIELDS},10,1,2);
-        //registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.BISON.getRegistryName().toString()),EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.PLAIN_FIELDS},8,2,4);
-        //registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.BASIC_BUTTERFLY1.getRegistryName().toString()),EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.PLAIN_FIELDS},10,1,1);
-        //registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.BASIC_BUTTERFLY2.getRegistryName().toString()),EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.PLAIN_FIELDS},10,1,1);
-        //registerEntitySpawn((EntityType<? extends EntityLiving>) EntityType.getById(ModEntities.PHASM.getRegistryName().toString()),EnumCreatureType.MONSTER,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_SWAMP},2,1,1);
+        registerEntitySpawn(ModEntities.MYSTIC_BIRD,EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST,ModBiomes.KATHARIAN_SWAMP,ModBiomes.PLAIN_FIELDS},12,1,2);
+        registerEntitySpawn(ModEntities.BIG_TURTLE,EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_DESERT,ModBiomes.KATHARIAN_DESERT_EDGE,ModBiomes.SOFT_SAND_LAKES},12,1,1);
+        registerEntitySpawn(ModEntities.POISONOUS_SCORPION,EnumCreatureType.MONSTER,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_DESERT,ModBiomes.KATHARIAN_DESERT_EDGE,ModBiomes.SOFT_SAND_LAKES},3,1,1);
+        registerEntitySpawn(ModEntities.CAMEL,EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_DESERT,ModBiomes.KATHARIAN_DESERT_EDGE,ModBiomes.SOFT_SAND_LAKES},6,1,1);
+        registerEntitySpawn(ModEntities.GECKO,EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST},4,1,1);
+        registerEntitySpawn(ModEntities.LIVING_FLOWER,EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST},8,1,1);
+        registerEntitySpawn(ModEntities.HOWLER,EnumCreatureType.MONSTER,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST},5,1,1);
+        registerEntitySpawn(ModEntities.FUNGITE,EnumCreatureType.MONSTER,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST},2,1,1);
+        //registerEntitySpawn(ModEntities.CACTI_SPORE,EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_FOREST},4,1,1);
+        //registerEntitySpawn(ModEntities.JELLY_FISH,EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.PLAIN_FIELDS},10,1,2);
+        //registerEntitySpawn(ModEntities.BISON,EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.PLAIN_FIELDS},8,2,4);
+        //registerEntitySpawn(ModEntities.BASIC_BUTTERFLY1,EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.PLAIN_FIELDS},10,1,1);
+        //registerEntitySpawn(ModEntities.BASIC_BUTTERFLY2,EnumCreatureType.CREATURE,ON_GROUND,new Biome[]{ModBiomes.PLAIN_FIELDS},10,1,1);
+        //registerEntitySpawn(ModEntities.PHASM,EnumCreatureType.MONSTER,ON_GROUND,new Biome[]{ModBiomes.KATHARIAN_SWAMP},2,1,1);
     }
 }
