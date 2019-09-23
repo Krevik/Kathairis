2d7ffc6c14be10545fdcca0e6be38bf1aecb1ce3
DESKTOP-3KV8A1L\Krevik <kczyku.kw@gmail.com>
2019-09-23 08:12:40 +0200

    Fixed some of the kathairis food

--- src/main/java/io/github/krevik/kathairis/init/ModFoods.java
+++ src/main/java/io/github/krevik/kathairis/init/ModFoods.java
@@ -12,5 +12,9 @@ public class ModFoods {
     public static final Food MAGIC_BEANS = (new Food.Builder().hunger(2).saturation(0.4f).setAlwaysEdible()).build();
     public static final Food NECTAR_BOWL = (new Food.Builder().hunger(2).saturation(0.8f).setAlwaysEdible()).build();
     public static final Food FUNGAL_DRUG = (new Food.Builder().hunger(2).saturation(0.3f).setAlwaysEdible().effect(new EffectInstance(Effects.NAUSEA,100,2),1f)).build();
+    public static final Food EASTER_FOOD = (new Food.Builder().hunger(2).saturation(0.25f).setAlwaysEdible()).build();
+    public static final Food COTTON_CANDY = (new Food.Builder().hunger(3).saturation(0.5f).setAlwaysEdible()).build();
+    public static final Food BISON_MEAT = (new Food.Builder().hunger(5).saturation(0.7f).setAlwaysEdible()).build();
+    public static final Food COOKED_BISON_MEAT = (new Food.Builder().hunger(6).saturation(1.0f).setAlwaysEdible()).build();
 
 }
